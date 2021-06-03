3764e02ac52bbb075924d1b07e752e8c56b1640f
Mika <mikeeverdel@gmail.com>
2021-06-03 14:55:07 +0200

    lose pygame, trying matplotlib

--- test_grid.py
+++ test_grid.py
@@ -1,26 +1,15 @@
-import pygame, sys
-from pygame.locals import*
+import numpy as np
+import matplotlib.pyplot as plt
 
-import os
-os.environ['SDL_VIDEODRIVER']='windlib'
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
 
-pygame.init()
-SCREENWIDTH = 800
-SCREENHEIGHT = 800
-RED = (255,0,0)
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+plt.plot(x, y)
 
-pygame.draw.rect(screen, RED, (400, 400, 20, 20),0)
-screen.fill(RED)
+plt.grid()
 
-pygame.display.update()
-
-# waint until user quits
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+plt.show()
