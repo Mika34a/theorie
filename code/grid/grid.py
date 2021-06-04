da14f47cb5969d37348c0e787767008d1d1baa10
Mika <mikeeverdel@gmail.com>
2021-06-04 15:09:26 +0200

    pygame file deleted

--- code/grid/grid.py
+++ code/grid/grid.py
@@ -34,14 +34,18 @@ plt.plot(50, 50)
 # loop for houses
 with open('database/district_1/district-1_houses.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    # skip first line
     next(plots)
     for row in plots:
         plt.plot(int(row[0]), int(row[1]), 'o')
 
+# loop for batteries
 with open('database/district_1/district-1_batteries.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    # skip first line
     next(plots)
     for row in plots:
+        # cleanup datafile
         x_cor, y_cor = row[0].split(',')
         plt.plot(int(x_cor), int(y_cor), 'Pr')
 
